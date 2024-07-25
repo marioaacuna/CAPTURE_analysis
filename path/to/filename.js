777c12b2b2b1d2a112ca232c740cb7510b6ff86e
marioaacuna <36881679+marioaacuna@users.noreply.github.com>
2024-07-26 00:04:09 +0200

    delete added file by mistake

--- path/to/filename.js
+++
@@ -1,7 +0,0 @@
-// This is the preamble for the file
-// it describes what this code does and how to use it.
-
-// The following function calculates the sum of two numbers
-function add(a, B) {
-    return A + B;
-}
